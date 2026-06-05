4694dbf83b85bf39b99a19083e3ba214c95cee9b
Dorian URBANIAK <jjtgwa5ywt9dv4bzktkk24@gmail.com>
2026-06-05 13:47:41 +0200

    sw fix

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,9 @@ const CACHE_NAME = "undercover-v1";
 const urlsToCache = [
 	"/",
 	"/index.html",
-	"/src/script.js",
-	"/src/styles.css",
-	"/data/words.json",
+	"/script.js",
+	"/styles.css",
+	"/words.json",
 ];
 
 // Mise à jour du cache à chaque installation
